884cb33c8c2a358be93588b3e98be850865e08cb
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-06-28 23:59:11 +0200

    adding mdcss plugin to transpile-task

--- gulp.transpile.js
+++ gulp.transpile.js
@@ -2,7 +2,10 @@ const gulp = require('gulp');
 const postcss = require('postcss');
 
 gulp.task('sort', function () {
-  return gulp.src('src/**/*.sss')
+  return gulp.src('src/**/*.sss.md')
+    .pipe(postcss([
+      require('mdcss')({ theme: require('mdcss-theme-github') })
+    ])
     .pipe(postcss(sugarss, { stringifier: css }))
     .pipe(gulp.dest('css'));
 });
